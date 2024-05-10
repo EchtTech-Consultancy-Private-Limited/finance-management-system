0b27963953383727c44bdb219eb1e09da737eced
Gireesh <gireesh.shukla@echttech.com>
2024-05-10 16:02:07 +0530

    add Eye icon with functionality in login pager

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="zxx">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="baseURL" content="{{ asset('/') }}">
     <title>Financial Management System | Login</title>
-    <link rel="shortcut icon" href="{{ asset('assets-cms/media/logos/favicon.ico') }}"/>
+    <link rel="shortcut icon" href="{{ asset('assets-cms/media/logos/favicon.ico') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap1.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/finance-nrcp/themefy_icon/themify-icons.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/finance-nrcp/font_awesome/css/all.min.css') }}" />
@@ -18,6 +19,7 @@
     <script src="{{ asset('assets/js/captcharefresh.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('assets/css/colors/default.css') }}" id="colorSkinCSS">
 </head>
+
 <body class="crm_body_bg">
     <section class="main_content login-p ps-0 p-0">
         <div class="main_content_iner p-0">
@@ -34,46 +36,63 @@
                                         {!! Toastr::message() !!}
                                         <div class="modal-body">
                                             <form method="POST" action="{{ route('authenticate') }}">
-                                            @csrf
-                                            <div class="form-check">
-                                                <div class="radio-btn-card d-flex mb-3">
-                                                    <div class="d-flex me-5">
-                                                        <input class="form-check-radio" type="radio" name="usertype" id="gridRadios1" value="0" checked="">
-                                                        <label class="form-label form-check-label" for="gridRadios1">National User</label>
+                                                @csrf
+                                                <div class="form-check">
+                                                    <div class="radio-btn-card d-flex mb-3">
+                                                        <div class="d-flex me-5">
+                                                            <input class="form-check-radio" type="radio" name="usertype"
+                                                                id="gridRadios1" value="0" checked="">
+                                                            <label class="form-label form-check-label"
+                                                                for="gridRadios1">National User</label>
+                                                        </div>
+                                                        <div class="d-flex">
+                                                            <input class="form-check-radio" type="radio" name="usertype"
+                                                                id="gridRadios2" value="1">
+                                                            <label class="form-label form-check-label"
+                                                                for="gridRadios2">Institute User</label>
+                                                        </div>
                                                     </div>
-                                                    <div class="d-flex">
-                                                        <input class="form-check-radio" type="radio" name="usertype" id="gridRadios2" value="1">
-                                                        <label class="form-label form-check-label" for="gridRadios2">Institute User</label>
-                                                     </div>
-                                                </div>
-                                                <div class>
-                                                    <input type="text" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" placeholder="Enter your email">
-                                                    @error('email')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                    @enderror
-                                                </div>
-                                                <div class>
-                                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
-                                                    @error('password')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                    @enderror
-                                                </div>
-                                                <div class>
-                                                    <img src="{{ url('captcha-code') }}" id="captchaimg" height="50" width="350">
-                                                </div>
-                                                <fieldset class="mb-3">
-                                                    Can't read the above code? <a class="ccc" href="javascript:void(0);" onClick="refreshCaptcha();">Refresh</a>
-                                                </fieldset>
-                                                <fieldset>
-                                                    <input placeholder="Enter Captcha" type="text" name="captcha_code" tabindex="2" id="captcha_code">
-                                                    @error('captcha_code')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                    @enderror
-                                                </fieldset>
-                                                 <button type="submit" class="btn_1 full_width text-center">Log in</button>
-                                                <div class="text-center">
-                                                    <a href="#" data-toggle="modal" data-target="#forgot_password" data-dismiss="modal" class="pass_forget_btn">Forget Password?</a>
-                                                </div>
+                                                    <div class>
+                                                        <input type="text" name="email" value="{{ old('email') }}"
+                                                            class="form-control @error('email') is-invalid @enderror"
+                                                            placeholder="Enter your email">
+                                                        @error('email')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="p-relative">
+                                                        <input type="password" name="password"
+                                                            class="form-control @error('password') is-invalid @enderror"
+                                                            placeholder="Password" id="pass_log_id">
+                                                        <span toggle="#password-field"
+                                                            class="fa fa-fw fa-eye field_icon toggle-password icon-psw"></span>
+                                                        @error('password')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                        @enderror
+                                                    </div>
+                                                    <div class>
+                                                        <img src="{{ url('captcha-code') }}" id="captchaimg" height="50"
+                                                            width="350">
+                                                    </div>
+                                                    <fieldset class="mb-3">
+                                                        Can't read the above code? <a class="ccc"
+                                                            href="javascript:void(0);"
+                                                            onClick="refreshCaptcha();">Refresh</a>
+                                                    </fieldset>
+                                                    <fieldset>
+                                                        <input placeholder="Enter Captcha" type="text"
+                                                            name="captcha_code" tabindex="2" id="captcha_code">
+                                                        @error('captcha_code')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                        @enderror
+                                                    </fieldset>
+                                                    <button type="submit" class="btn_1 full_width text-center">Log
+                                                        in</button>
+                                                    <div class="text-center">
+                                                        <a href="#" data-toggle="modal" data-target="#forgot_password"
+                                                            data-dismiss="modal" class="pass_forget_btn">Forget
+                                                            Password?</a>
+                                                    </div>
                                             </form>
                                         </div>
                                     </div>
@@ -96,6 +115,19 @@
             </div>
         </div> -->
     </section>
+
+    <script>
+    $(".toggle-password.icon-psw").click(function() {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        input = $(this).parent().find("input");
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+    </script>
+
     <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
     <script src="{{ asset('assets/js/popper1.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap1.min.js') }}"></script>
@@ -104,4 +136,5 @@
     <script src="{{ asset('assets/finance-nrcp/scroll/scrollable-custom.js') }}"></script>
     <script src="{{ asset('assets/js/custom.js') }}"></script>
 </body>
+
 </html>
