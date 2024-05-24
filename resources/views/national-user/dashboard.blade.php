78472c05845f37d2f1ccb6eae5b7b4f6d02d5aa6
Gireesh <gireesh.shukla@echttech.com>
2024-05-24 13:58:29 +0530

    Update Dashboard ui

--- resources/views/national-user/dashboard.blade.php
+++ resources/views/national-user/dashboard.blade.php
@@ -24,8 +24,8 @@
                 <div class="page_date_button d-flex align-items-center">
                     <img src="{{ asset('assets/img/icon/calender_icon.svg') }}" alt>
                     @php
-                        $first_date = date('F d ,Y',strtotime('first day of this month'));
-                        $last_date = date('F d ,Y',strtotime('last day of this month'));
+                    $first_date = date('F d ,Y',strtotime('first day of this month'));
+                    $last_date = date('F d ,Y',strtotime('last day of this month'));
                     @endphp
                     {{ $first_date }} - {{ $last_date }}
                 </div>
@@ -38,29 +38,66 @@
 
 
     <div class="col-xl-12 white_card card_height_100 user_crm_wrapper">
-        <div class="crad white_card mb_30 p-4 total-card">
+        <div class="crad mb_30 p-2 pe-0 total-card">
             <div class="row">
-                <div class="col-md-4">
-                    <div>
+                <div class="col-md-9 row p-0">
 
-                        <h3>Total State + UT: 36 </h3>
-                        <h3>Total Sentinel Site: 45 </h3>
-                        <h3>Total PPCL Labs: 6 </h3>
-
+                    <div class="col-md-4 mb-4">
+                        <div class="profile-text">
+                            <div class="school-info-box">
+                                <p>Total State + UT</p>
+                                <span class="studentNumber">36</span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div>
-
-                        <h3>Total Regional Coordinator: 15 </h3>
-                        <h3>Total NRCP Labs: 10 </h3>
-                        <h3>Total PM ABHIM SSS: 12 </h3>
-
+                    <div class="col-md-4 mb-4">
+                        <div class="profile-text">
+                            <div class="school-info-box">
+                                <p>Total Sentinel Site</p>
+                                <span class="studentNumber">45</span>
+                            </div>
+                        </div>
                     </div>
+                    <div class="col-md-4 mb-4">
+                        <div class="profile-text">
+                            <div class="school-info-box">
+                                <p>Total PPCL Labs</p>
+                                <span class="studentNumber">6</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="profile-text">
+                            <div class="school-info-box">
+                                <p>Total Regional Coordinator</p>
+                                <span class="studentNumber">15</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="profile-text">
+                            <div class="school-info-box">
+                                <p>Total NRCP Labs</p>
+                                <span class="studentNumber">10</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="profile-text">
+                            <div class="school-info-box">
+                                <p>Total PM ABHIM SSS</p>
+                                <span class="studentNumber">12</span>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div class="col-md-4">
-                    <div class="total-card-child d-flex align-items-center justify-content-center">
-                        <h3>Total Value</h3>
+
+                <div class="col-md-3 pe-0">
+                    <div class="white_card graph-card-h">
+                        <div class="total-card-child d-flex align-items-center justify-content-center">
+                            <h3>Total Value</h3>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -69,12 +106,12 @@
     </div>
 
     <div class="col-xl-12 white_card  user_crm_wrapper">
-        <div class="crad white_card mb_30 p-4 fund-card">
+        <div class="crad mb_30 p-2 fund-card">
             <div class="row">
                 <div class="col-md-3">
                     <div class="fund-card-child">
                         <h3>
-                            Fund approved (in Cr.)
+                            Fund approved <br> (in Cr.)
                         </h3>
                         <span class="fund-number">
                             {{ @$totalArray['unspentBalance1stTotal'] }}
@@ -85,7 +122,7 @@
                 <div class="col-md-3">
                     <div class="fund-card-child">
                         <h3>
-                            Fund Expenditure (in Cr.)
+                            Fund Expenditure <br> (in Cr.)
                         </h3>
                         <span class="fund-number">
                             {{ @$totalArray['actualExpenditureTotal'] }}
@@ -95,7 +132,7 @@
                 <div class="col-md-3">
                     <div class="fund-card-child">
                         <h3>
-                            Financial Progress (%)
+                            Financial Progress <br> (%)
                         </h3>
                         <span class="fund-number">
                             85%
@@ -105,7 +142,7 @@
                 <div class="col-md-3">
                     <div class="fund-card-child">
                         <h3>
-                            Balance Amount (in Cr.)
+                            Balance Amount <br> (in Cr.)
                         </h3>
                         <span class="fund-number">
                             15%
@@ -178,17 +215,22 @@
                         </div>
                     </div>
 
+                    <div class="col-md-6 col-lg-4 choose-financial-year-select">
+                        <div class="d-flex align-items-center">
+                            <label for="" class="text-nowrap me-3 font-16"><b>Program Wise <sup
+                                        class="text-danger">*</sup></b></label>
+                            <select id="national-user-fy" name="financial_year" class="form-control">
+                                <option value="">Select Program</option>
+                                <option value="">Program 1</option>
+                            </select>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-            <div class="row card-mm">
+            <div class="row card-mm">              
                 <div class="col-md-4 col-lg">
-                    <div class="single_crm border-line-1 p-0">
-                        <div class="crm_head d-flex align-items-center justify-content-between">
-                            <div class="thumb">
-                                <i class="fas fa-calendar f_s_16 white_text"></i>
-                            </div>
-                            <i class="fas fa-ellipsis-h f_s_11 white_text"></i>
-                        </div>
+                    <div class="single_crm border-line-1 p-0">                      
                         <div class="crm_body">
                             <h4 id="national-giaReceivedTotal">{{ @$totalArray['giaReceivedTotal'] }}</h4>
                             <p>GIA Received during the Current F.Y. </p>
@@ -196,13 +238,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 col-lg">
-                    <div class="single_crm border-line-2 p-0">
-                        <div class="crm_head crm_bg_1 d-flex align-items-center justify-content-between">
-                            <div class="thumb">
-                                <i class="fas fa-credit-card f_s_16 white_text"></i>
-                            </div>
-                            <i class="fas fa-ellipsis-h f_s_11 white_text"></i>
-                        </div>
+                    <div class="single_crm border-line-2 p-0">                       
                         <div class="crm_body">
                             <h4 id="national-committedLiabilitiesTotal">{{ @$totalArray['committedLiabilitiesTotal'] }}
                             </h4>
@@ -211,13 +247,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 col-lg">
-                    <div class="single_crm border-line-3 p-0">
-                        <div class="crm_head crm_bg_2 d-flex align-items-center justify-content-between">
-                            <div class="thumb">
-                                <i class="fas fa-plus f_s_16 white_text"></i>
-                            </div>
-                            <i class="fas fa-ellipsis-h f_s_11 white_text"></i>
-                        </div>
+                    <div class="single_crm border-line-3 p-0">                       
                         <div class="crm_body">
                             <h4 id="national-totalBalanceTotal">{{ @$totalArray['totalBalanceTotal'] }}</h4>
                             <p>Total Balance excluding interest</p>
@@ -225,35 +255,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 col-lg">
-                    <div class="single_crm border-line-4 p-0">
-                        <div class="crm_head crm_bg_3 d-flex align-items-center justify-content-between">
-                            <div class="thumb">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="15.535" height="14.888"
-                                    viewBox="0 0 15.535 14.888">
-                                    <g id="infographic" transform="translate(0 -1)">
-                                        <path id="Path_1511" data-name="Path 1511"
-                                            d="M2.647,7.65a.647.647,0,0,1-.43-1.131L5.13,3.93a.644.644,0,0,1,.507-.159l5.038.61,2.854-2.7a.647.647,0,0,1,.89.941L11.345,5.531a.65.65,0,0,1-.523.173l-5.05-.613-2.695,2.4a.645.645,0,0,1-.43.163Z"
-                                            transform="translate(-0.706 -0.177)" fill="#fff"></path>
-                                        <path id="Path_1512" data-name="Path 1512"
-                                            d="M19.751,4.236a.483.483,0,0,1-.343-.142L17.143,1.829A.485.485,0,0,1,17.486,1h2.266a.486.486,0,0,1,.485.485V3.751a.485.485,0,0,1-.485.485Z"
-                                            transform="translate(-5.996)" fill="#fff"></path>
-                                        <path id="Path_1513" data-name="Path 1513"
-                                            d="M4.884,17.809v1.78H1v-1.78A.809.809,0,0,1,1.809,17H4.075A.809.809,0,0,1,4.884,17.809Z"
-                                            transform="translate(-0.353 -5.643)" fill="#fff"></path>
-                                        <path id="Path_1514" data-name="Path 1514"
-                                            d="M12.884,11.809v5.664H9V11.809A.809.809,0,0,1,9.809,11h2.266A.809.809,0,0,1,12.884,11.809Z"
-                                            transform="translate(-3.174 -3.527)" fill="#fff"></path>
-                                        <path id="Path_1515" data-name="Path 1515"
-                                            d="M20.884,13.809v4.369H17V13.809A.809.809,0,0,1,17.809,13h2.266A.809.809,0,0,1,20.884,13.809Z"
-                                            transform="translate(-5.996 -4.232)" fill="#fff"></path>
-                                        <path id="Path_1516" data-name="Path 1516"
-                                            d="M15.05,23.471H.485a.485.485,0,1,1,0-.971H15.05a.485.485,0,1,1,0,.971Z"
-                                            transform="translate(0 -7.583)" fill="#fff"></path>
-                                    </g>
-                                </svg>
-                            </div>
-                            <i class="fas fa-ellipsis-h f_s_11 white_text"></i>
-                        </div>
+                    <div class="single_crm border-line-4 p-0">                        
                         <div class="crm_body">
                             <h4 id="national-actualExpenditureTotal">{{ @$totalArray['actualExpenditureTotal'] }}</h4>
                             <p>Actual Expenditure incurred during the current F.Y </p>
@@ -261,37 +263,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 col-lg">
-                    <div class="single_crm border-line-5 p-0">
-                        <div class="crm_head crm_bg_4 d-flex align-items-center justify-content-between">
-                            <div class="thumb">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="15.412" height="15.412"
-                                    viewBox="0 0 15.412 15.412" class="briteness-10">
-                                    <g id="XMLID_17_" transform="translate(0 -0.002)">
-                                        <path id="XMLID_40_" d="M257.823.4A7.874,7.874,0,0,0,255,0V3.221Z"
-                                            transform="translate(-247.294 0)" fill="#5e3787"></path>
-                                        <path id="XMLID_560_"
-                                            d="M406.712,164.428a7.873,7.873,0,0,0-.4-2.823l-2.823,2.823Z"
-                                            transform="translate(-391.3 -156.719)" fill="#5e3787"></path>
-                                        <path id="XMLID_561_"
-                                            d="M255,65.9h.641l5.252-5.252a7.285,7.285,0,0,0-.641-.641L255,65.263Z"
-                                            transform="translate(-247.294 -58.195)" fill="#5e3787"></path>
-                                        <path id="XMLID_562_"
-                                            d="M319.921,109.629l3.724-3.724a7.253,7.253,0,0,0-.475-.807l-4.531,4.531Z"
-                                            transform="translate(-309.01 -101.92)" fill="#5e3787"></path>
-                                        <path id="XMLID_2261_"
-                                            d="M255,30.729,259.53,26.2a7.238,7.238,0,0,0-.807-.476L255,29.447Z"
-                                            transform="translate(-247.294 -24.943)" fill="#5e3787"></path>
-                                        <path id="XMLID_2337_"
-                                            d="M6.8,29.884l-.513.068A7.254,7.254,0,0,0,1.813,41.939L6.8,36.953Z"
-                                            transform="translate(0 -28.979)" fill="#5e3787"></path>
-                                        <path id="XMLID_2340_"
-                                            d="M81.205,290.006a7.254,7.254,0,0,0,11.987-4.473l.068-.513H86.191Z"
-                                            transform="translate(-78.751 -276.405)" fill="#5e3787"></path>
-                                    </g>
-                                </svg>
-                            </div>
-                            <i class="fas fa-ellipsis-h f_s_11 white_text"></i>
-                        </div>
+                    <div class="single_crm border-line-5 p-0">                      
                         <div class="crm_body">
                             <h4 id="national-unspentBalance31stTotal">{{ @$totalArray['unspentBalance31stTotal'] }}</h4>
                             <p>Unspent Balance (excluding Interest ) </p>
