e910085a86d902a124ad12c362a0efb942698d60
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-08-08 11:30:31 +0530

    prechanges

--- resources/views/institute-user/SOEUC/create.blade.php
+++ resources/views/institute-user/SOEUC/create.blade.php
@@ -111,6 +111,8 @@
                             @enderror
                         </div>
 
+
+
                         
                         <div class="col-md-4 mb-4">
                             <label class="form-label" for="inputAddress2">Nodal/Program Officer Mobile<span
